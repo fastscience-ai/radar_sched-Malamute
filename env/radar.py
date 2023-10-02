bf6bba85569767bd7ce09d432939ce25eb3e5e0b
Vidyasagar Sadhu <srikanthvidyasagar.sadhu@sri.com>
2023-10-02 16:58:08 -0700

    Brought in changes from Paul's latest scenesim to Gym wrapper along with changing vel_mean and vel_sigma to integers

--- env/radar.py
+++ env/radar.py
@@ -8,10 +8,10 @@ import numpy as np
 import simpy
 
 class Target(object):
-    """A target either moves at a constant speed or sits stationary.
-
+    """
+    Define target class with all properties necessary for target to move or remain stationary in grid.
+    A target either moves at a constant speed or sits stationary.
     A target has a *name*, a *Position*, a *velocity*, and a *state* (moving or stationary).
-
     """
     def __init__(self, sim, name, posx, posy, velx, vely, state):
         self.sim = sim
@@ -27,12 +27,19 @@ class Target(object):
         self.MOV_SIGMA = 2.0         # Sigma of target moving time
 
     def movetime(self):
+        # Time that target spends moving
         return random.normalvariate(self.MOV_MEAN, self.MOV_SIGMA)
 
     def stattime(self):
+        # Time that target spends stationary
         return random.normalvariate(self.STA_MEAN, self.STA_SIGMA)
 
     def time_to_interrogate(self):
+        # Set interrogation time - without this, moving targets will not have
+        # their locations update.
+        # This is because simpy is interrupt-driven.  Without interrrupts processes
+        # do not change the state of the targets until their move or stationary
+        # time is finished.
         self.done_in2 = 1
         return self.done_in2
 
@@ -45,8 +52,11 @@ class Target(object):
 
         """
         while True:
-            # Start moving
-            done_in = self.movetime()
+            # Begin by setting time for current action given target state
+            if self.state == 1:
+                done_in = self.movetime()
+            else:
+                done_in = self.stattime()
             while done_in:
                 try:
                     # Move the target
@@ -77,6 +87,9 @@ class Target(object):
                     done_in = 0  # Set to 0 to exit while loop.
 
                 except simpy.Interrupt:
+                    # when interrupt received, update target position and
+                    # then reset time remaining for current action based on
+                    # interrupt time
                     posx = posx + (self.sim.now - start) * vx
                     posy = posy + (self.sim.now - start) * vy
                     print("Target state at interrupt time %.2f :" %self.sim.now)
@@ -86,7 +99,8 @@ class Target(object):
                     self.posy = posy
 
     def interrogate(self) :
-        #Interrogate evolving targets to obtain their current state and position
+        # Interrogate evolving targets to obtain their current state and position
+        # Here is where the interrupt is sent
         while True:
             yield self.sim.timeout(self.time_to_interrogate())
 
@@ -94,27 +108,32 @@ class Target(object):
     def done_in2(self):
         return self.done_in2
     def __repr__(self):
+        # Define the outputs to be sent to the simulation for further processing
         return "Target,%d,state,%d,xpos,%.2f,ypos,%.2f" % (self.name, self.state, self.posx, self.posy)
 
 
 
 class Radar(gym.Env):
     def __init__(self):
+        # Constants needed for code
+        # Values set for testing
+        # More typical values given in commented lines
         self.RANDOM_SEED = 45
         self.NUM_TARGET = 2          # Number of targets
+        # self.NUM_TARGET = 12         # Number of targets for initial training, could be 20
         self.MOV_MEAN = 10.0         # Avg. target moving time in minutes
         self.MOV_SIGMA = 2.0         # Sigma of target moving time
         self.STA_MEAN = 15.0         # Avg. target stationary time in minutes
         self.STA_SIGMA = 3.0         # Sigma of target stationary time
-        self.VEL_MEAN = 2.0          # Avg. target moving velocity
-        self.VEL_SIGMA = 1.0         # Sigma of target moving velocity
+        self.VEL_MEAN = 2            # Avg. target moving velocity
+        self.VEL_SIGMA = 1           # Sigma of target moving velocity
         self.GRID_SIZE = 800         # Size of grid on which targets move
         self.MID_GRID  = 400         # Center of grid
         self.GUARD_BAND = 100        # Size of guard band in which targets move but are not detected
         self.MAX_STRENGTH = 30       # Maximum strength of target (fixed for now)
         self.STRENGTH_INC = 10       # Strength increase for target which is detected (fixed for now)
         self.COAST_PEN = 1           # Penalty for coasting (i.e. not detecting) a target in a given time step
-        self.SIM_TIME = 5            # Length of simulation1
+        self.SIM_TIME = 50            # Length of simulation1
         self.target_info_current = np.zeros((self.NUM_TARGET,5))
         self.target_info_prior = np.zeros((self.NUM_TARGET,5))
         self.viewer = None
@@ -133,13 +152,20 @@ class Radar(gym.Env):
 
     def step(self, int_type):
         print("action\n\n\n\n\n",int_type) 
-        # Execution
+        # Step the simulation
         simtime = 0
+        # the following three arrays keep track of target parameters extracted from
+        # interrogating the targets
         targetname = np.zeros((self.NUM_TARGET),dtype='int')
         targetmode = np.zeros((self.NUM_TARGET),dtype='int')
         targetpos = np.zeros((self.NUM_TARGET,2),dtype='float')
         while simtime < self.SIM_TIME :
+            # update the prior information array
             self.target_info_prior = self.target_info_current
+            self.target_info_current[:, 5] = 0  # set all targets as not detected in current interrogation
+            # For this simulation, pick actions for agent at random,
+            # in RL system, training will focus on picking actions to maximize
+            # information gain
             if int_type < 8 :
                 print("MTI mode chosen")
                 done_in2 = 1
@@ -149,6 +175,7 @@ class Radar(gym.Env):
             else :
                 print("Self test mode chosen")
                 done_in2 = 2
+            # set time for simulation to be interrogated
             simtime += done_in2
             #SOO
             #try: 
@@ -157,6 +184,7 @@ class Radar(gym.Env):
             #    simtime+=1
             self.sim.run(until=simtime)
             print("At time = %d" % simtime)
+            # print out target information string defined in class target
             print([self.targets])
             # Use printed target string to generate current target information
             tstring = str([self.targets])
@@ -171,65 +199,73 @@ class Radar(gym.Env):
             # Interrogate targets to upate detections
             for i in range(self.NUM_TARGET) :
                 if int_type < 2 :           #MTI Mode Lower left portion of grid
-                    if targetmode[i] == 0 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= self.MID_GRID and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= self.MID_GRID:
-                        self.target_info_current[i,1] = 0
-                        self.target_info_current[i,3] = targetpos[i,0]
-                        self.target_info_current[i,4] = targetpos[i,1]
-                        if self.target_info_prior[i,1] == 1 :
-                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
-                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
-                            self.target_info_current[i,2] += self.STRENGTH_INC
-                        else :
-                            self.target_info_current[i,2] = self.MAX_STRENGTH
-                elif int_type < 4 :         #MTI Mode Upper left portion of grid
-                    if targetmode[i] == 0 and targetpos[i,0] > self.MID_GRID and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= self.MID_GRID:
-                        self.target_info_current[i,1] = 0
-                        self.target_info_current[i,3] = targetpos[i,0]
-                        self.target_info_current[i,4] = targetpos[i,1]
-                        if self.target_info_prior[i,1] == 1 :
-                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
-                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
-                            self.target_info_current[i,2] += self.STRENGTH_INC
-                        else :
-                            self.target_info_current[i,2] = self.MAX_STRENGTH
-                elif int_type < 6 :         #MTI Mode Lower right portion of grid
-                    if targetmode[i] == 0 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= self.MID_GRID and targetpos[i,1] > self.MID_GRID and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
-                        self.target_info_current[i,1] = 0
-                        self.target_info_current[i,3] = targetpos[i,0]
-                        self.target_info_current[i,4] = targetpos[i,1]
-                        if self.target_info_prior[i,1] == 1 :
-                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
-                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
-                            self.target_info_current[i,2] += self.STRENGTH_INC
-                        else :
-                            self.target_info_current[i,2] = self.MAX_STRENGTH
-
-                elif int_type < 8:          #MTI Mode Upper right portion of grid
-                    if targetmode[i] == 0 and targetpos[i,0] > self.MID_GRID and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] > self.MID_GRID and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
-                        self.target_info_current[i,1] = 0
-                        self.target_info_current[i,3] = targetpos[i,0]
-                        self.target_info_current[i,4] = targetpos[i,1]
-                        if self.target_info_prior[i,1] == 1 :
-                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
-                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
-                            self.target_info_current[i,2] += self.STRENGTH_INC
-                        else :
-                            self.target_info_current[i,2] = self.MAX_STRENGTH
-
-                elif int_type < 10 :
-                    if targetmode[i] == 1 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
+                    if targetmode[i] == 1 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= self.MID_GRID and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= self.MID_GRID:
                         self.target_info_current[i,1] = 1
                         self.target_info_current[i,3] = targetpos[i,0]
                         self.target_info_current[i,4] = targetpos[i,1]
                         if self.target_info_prior[i,1] == 0 :
+                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
+                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
+                            self.target_info_current[i,2] += self.STRENGTH_INC
+                        else :
+                            self.target_info_current[i,2] = self.MAX_STRENGTH
+                        self.target_info_current[i,5] = 1 #mark this target as currently detected
+
+                elif int_type < 4 :         #MTI Mode Upper left portion of grid
+                    if targetmode[i] == 1 and targetpos[i,0] > self.MID_GRID and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= self.MID_GRID:
+                        self.target_info_current[i,1] = 1
+                        self.target_info_current[i,3] = targetpos[i,0]
+                        self.target_info_current[i,4] = targetpos[i,1]
+                        if self.target_info_prior[i,1] == 0 :
+                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
+                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
+                            self.target_info_current[i,2] += self.STRENGTH_INC
+                        else :
+                            self.target_info_current[i,2] = self.MAX_STRENGTH
+                        self.target_info_current[i,5] = 1 #mark this target as currently detected
+
+                elif int_type < 6 :         #MTI Mode Lower right portion of grid
+                    if targetmode[i] == 1 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= self.MID_GRID and targetpos[i,1] > self.MID_GRID and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
+                        self.target_info_current[i,1] = 1
+                        self.target_info_current[i,3] = targetpos[i,0]
+                        self.target_info_current[i,4] = targetpos[i,1]
+                        if self.target_info_prior[i,1] == 0 :
+                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
+                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
+                            self.target_info_current[i,2] += self.STRENGTH_INC
+                        else :
+                            self.target_info_current[i,2] = self.MAX_STRENGTH
+                        self.target_info_current[i,5] = 1 #mark this target as currently detected
+
+                elif int_type < 8:          #MTI Mode Upper right portion of grid
+                    if targetmode[i] == 1 and targetpos[i,0] > self.MID_GRID and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] > self.MID_GRID and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
+                        self.target_info_current[i,1] = 1
+                        self.target_info_current[i,3] = targetpos[i,0]
+                        self.target_info_current[i,4] = targetpos[i,1]
+                        if self.target_info_prior[i,1] == 0 :
+                            self.target_info_current[i,2] = 0      # if target was stationary when last interrogated, reset target strength to 0
+                        if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
+                            self.target_info_current[i,2] += self.STRENGTH_INC
+                        else :
+                            self.target_info_current[i,2] = self.MAX_STRENGTH
+                        self.target_info_current[i,5] = 1 #mark this target as currently detected
+
+                elif int_type < 10:         # SAR Mode, applies to entire grid
+                    if targetmode[i] == 0 and targetpos[i,0] >= self.GUARD_BAND and targetpos[i,0] <= (self.GRID_SIZE - self.GUARD_BAND) and targetpos[i,1] >= self.GUARD_BAND and targetpos[i,1] <= (self.GRID_SIZE - self.GUARD_BAND):
+                        self.target_info_current[i,1] = 0
+                        self.target_info_current[i,3] = targetpos[i,0]
+                        self.target_info_current[i,4] = targetpos[i,1]
+                        if self.target_info_prior[i,1] == 1 :
                             self.target_info_current[i,2] = 0      # if target was moving when last interrogated, reset target strength to 0
                         if self.target_info_current[i,2] < (self.MAX_STRENGTH - self.STRENGTH_INC) :
                             self.target_info_current[i,2] += self.STRENGTH_INC
                         else :
                             self.target_info_current[i,2] = self.MAX_STRENGTH
+                        self.target_info_current[i,5] = 1 #mark this target as currently detected
+
             # If a target not detected in this time step, apply coasting penalty
             for i in range(self.NUM_TARGET) :
-                if self.target_info_current[i,2] == self.target_info_prior[i,2] :
+                if self.target_info_current[i,5] == 0 :
                     if self.target_info_current[i,2] > self.COAST_PEN :
                         self.target_info_current[i,2] -= self.COAST_PEN
                     else :
@@ -241,6 +277,9 @@ class Radar(gym.Env):
     def reset(self):
         # Create a simulation and start the setup process
         self.sim = simpy.Environment()
+
+        # simtime keeps track of the simulation elapsed time
+        self.simtime = 0
         iposx = np.zeros((self.NUM_TARGET))
         iposy = np.zeros((self.NUM_TARGET))
         ivelx = np.zeros((self.NUM_TARGET))
@@ -250,21 +289,23 @@ class Radar(gym.Env):
         # keep both current target state and prior state for comparison
         # Indices:
         #  0: Target ID #
-        #  1: Target Movement ID# - 0=moving, 1=stationary
+        #  1: Target Movement ID# - 1=moving, 0=stationary
         #  2: Target detection strength
         #  3: Target x position
         #  4: Target y position
+        #  5: Target currently located - 0=no, 1=yes
         #  Note: while the defined position array is 800x800, 
         #        only the central 600x600 is valid for locating targets (i.e. is the search areas)
         #        This allows targets to enter search area without having to be specially initialized.
-        self.target_info_current = np.zeros((self.NUM_TARGET,5))
-        self.target_info_prior = np.zeros((self.NUM_TARGET,5))
+        self.target_info_current = np.zeros((self.NUM_TARGET,6))
+        self.target_info_prior = np.zeros((self.NUM_TARGET,6))
         #Initialize target ID #'s in both arrays - these will not change 
         for i in range(self.NUM_TARGET) :
             self.target_info_current[i,0] = i
             self.target_info_prior[i,0] = i
 
         # Randomly select initial values for targets
+        # Position, velocity, and state
         for i in range(self.NUM_TARGET) :
             iposx[i] = random.randint(0,self.GRID_SIZE)
             iposy[i] = random.randint(0,self.GRID_SIZE)
@@ -272,6 +313,7 @@ class Radar(gym.Env):
             ively[i] = random.randint(-self.VEL_MEAN - self.VEL_SIGMA, self.VEL_MEAN + self.VEL_SIGMA)
             istate[i] = random.randint(0,1)
 
+        # Initialize the targets
         self.targets = [Target(self.sim, i, iposx[i], iposy[i], ivelx[i], ively[i], istate[i] )
                     for i in range(self.NUM_TARGET)] 
         
